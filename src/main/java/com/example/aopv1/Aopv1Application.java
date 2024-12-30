d3f749908e27b5565386f3da88485970c1ab00db
badop <tanoth700@gmail.com>
2024-12-30 12:59:55 +0100

    @After, @AfterThrowing, @AfterReturning advices implemented and tested

--- src/main/java/com/example/aopv1/Aopv1Application.java
+++ src/main/java/com/example/aopv1/Aopv1Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class Aopv1Application {
 
@@ -17,10 +19,50 @@ public class Aopv1Application {
     @Bean
     public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO membershipDAO) {
         return runner -> {
-            beforeAdvice(theAccountDAO, membershipDAO);
-
+            //beforeAdvice(theAccountDAO, membershipDAO);
+            //afterReturningAdvice(theAccountDAO);
+            //afterThrowingAdvice(theAccountDAO);
+            afterAdvice(theAccountDAO);
         };
+    }
 
+    private void afterAdvice(AccountDAO theAccountDAO) {
+        List<Account> theAccounts = null;
+
+        try {
+            boolean breakCode = false;
+            theAccounts = theAccountDAO.findAccounts(breakCode);
+
+        } catch (Exception exception) {
+            System.out.println("Caught exception: " + exception);
+        }
+
+
+        System.out.println("Main program: afterThrowingAdvice");
+        System.out.println(theAccounts);
+    }
+
+    private void afterThrowingAdvice(AccountDAO theAccountDAO) {
+        List<Account> theAccounts = null;
+
+        try {
+            boolean breakCode = true;
+            theAccounts = theAccountDAO.findAccounts(breakCode);
+
+        } catch (Exception exception) {
+            System.out.println("Caught exception: " + exception);
+        }
+
+
+        System.out.println("Main program: afterThrowingAdvice");
+        System.out.println(theAccounts);
+    }
+
+    private void afterReturningAdvice(AccountDAO theAccountDAO) {
+        List<Account> theAccounts = theAccountDAO.findAccounts();
+
+        System.out.println("Main program: afterReturningAdvice");
+        theAccounts.forEach(System.out::println);
     }
 
     private void beforeAdvice(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
--- src/main/java/com/example/aopv1/dao/AccountDAOImpl.java
+++ src/main/java/com/example/aopv1/dao/AccountDAOImpl.java
@@ -3,6 +3,9 @@ package com.example.aopv1.dao;
 import com.example.aopv1.Account;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class AccountDAOImpl implements AccountDAO {
 
@@ -19,5 +22,30 @@ public class AccountDAOImpl implements AccountDAO {
 
     public void makeProgress() {
         System.out.println(getClass() + ", makeProgress()");
+    }
+
+    @Override
+    public List<Account> findAccounts() {
+        return findAccounts(false);
+    }
+
+    @Override
+    public List<Account> findAccounts(boolean breakCode) {
+
+        if (breakCode) {
+            throw new RuntimeException("EXCEPTION!!!");
+        }
+
+        List<Account> myAccounts = new ArrayList<>();
+
+        Account account1 = new Account("John", "First");
+        Account account2 = new Account("Major", "Third");
+        Account account3 = new Account("Kris", "Second");
+
+        myAccounts.add(account1);
+        myAccounts.add(account2);
+        myAccounts.add(account3);
+
+        return myAccounts;
     }
 }
